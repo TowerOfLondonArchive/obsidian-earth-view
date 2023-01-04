a52bf67e839474eb5d0ecc34da29add5e2919251
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-01-04 13:28:46 +0000

    Added the start of a global map view

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { Plugin } from 'obsidian';
 import './leaflet_config'
 import EarthCodeBlockManager from './embedded_map';
+import EarthViewManager from "./map_view";
 
 export default class EarthPlugin extends Plugin {
 	async onload() {
 		new EarthCodeBlockManager(this);
+		new EarthViewManager(this);
 	}
 }
