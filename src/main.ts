3c2c684b68f09ef162f47cffec5e316e85560097
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-01-08 16:09:12 +0000

    Improved map view

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { Plugin } from 'obsidian';
 import './leaflet_config'
 import EarthCodeBlockManager from './embedded_map';
 import EarthViewManager from "./map_view";
+import {Database} from "./database";
+import {LatLngBounds} from "leaflet"
 
 export default class EarthPlugin extends Plugin {
-	async onload() {
-		new EarthCodeBlockManager(this);
-		new EarthViewManager(this);
+	database: Database
+	earth_view_manager: EarthViewManager
+	code_block_manager: EarthCodeBlockManager
+	default_bounds: LatLngBounds
+
+	onload() {
+		this.database = new Database(this);
+		this.earth_view_manager = new EarthViewManager(this, this.database);
+		this.code_block_manager = new EarthCodeBlockManager(this);
+		this.default_bounds = new LatLngBounds([51.506745,-0.079402], [51.509576,-0.073798]);
 	}
 }
